d2ff3c145918d716f38949dd78f1f129a691e825
Adam C <acc1729@gmail.com>
2021-11-11 23:29:58 -0500

    Update callsite of std.mem.tokenize

--- src/tokenize.zig
+++ src/tokenize.zig
@@ -4,10 +4,10 @@ const expect = std.testing.expect;
 /// Pre-processes the input, putting a space between each ( and )
 fn tokenizePrepass(str: []const u8) []u8 {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    var neededSize1: usize = std.mem.replacementSize(u8, str, "(", " ( ");
+    const neededSize1: usize = std.mem.replacementSize(u8, str, "(", " ( ");
     var buff1 = gpa.allocator.alloc(u8, neededSize1) catch unreachable;
     _ = std.mem.replace(u8, str, "(", " ( ", buff1);
-    var neededSize2 = std.mem.replacementSize(u8, buff1, ")", " ) ");
+    const neededSize2 = std.mem.replacementSize(u8, buff1, ")", " ) ");
     var buff2 = gpa.allocator.alloc(u8, neededSize2) catch unreachable;
     _ = std.mem.replace(u8, buff1, ")", " ) ", buff2);
     return buff2;
@@ -15,10 +15,10 @@ fn tokenizePrepass(str: []const u8) []u8 {
 
 /// Returns an array of tokens from a string input. 
 pub fn tokenize(str: []const u8) ![][]const u8 {
-    var prepass = tokenizePrepass(str);
+    const prepass = tokenizePrepass(str);
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     var list = std.ArrayList([]const u8).init(&gpa.allocator);
-    var tokens = std.mem.tokenize(prepass, " ");
+    var tokens = std.mem.tokenize(u8, prepass, " ");
     while (true) {
         try list.append(tokens.next() orelse break);
     }
